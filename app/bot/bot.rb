e2377d42988170f2d4be2bdc383465153c5da0da
Kenza <kenza.dieye@gmail.com>
2018-05-23 04:05:17 +0200

    first bot with rails

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -5,5 +5,13 @@ include Facebook::Messenger
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
 Bot.on :message do |message|
-  message.reply(text: 'Hello, human!')
+  if message.text include? "Bonjour"
+    message.reply("Bien le bonjour l'ami !")
+  elsif message.text include? "Yo"
+    message.reply("Wesh ma gueule !")
+  elsif message.text("Salut")
+    message.reply("Salut salut mon petit loup ! :)")
+  else
+    message.reply("Ne pas hésiter à dire bonjour, je ne suis pas ton chien...")
+  end
 end
