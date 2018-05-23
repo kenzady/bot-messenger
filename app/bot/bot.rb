22cf2654e7597421fa0d9ea071f79af37a2e6632
Kenza <kenza.dieye@gmail.com>
2018-05-23 04:13:53 +0200

    fixing second response error

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -5,11 +5,11 @@ include Facebook::Messenger
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
 Bot.on :message do |message|
-  if message.text include? "Bonjour"
+  if message.text.include? "Bonjour"
     message.reply(text: "Bien le bonjour l'ami !")
-  elsif message.text include? "Yo"
+  elsif message.text.include? "Yo"
     message.reply(text: "Wesh ma gueule !")
-  elsif message.text("Salut")
+  elsif message.text.include? "Salut"
     message.reply(text: "Salut salut mon petit loup ! :)")
   else
     message.reply("Ne pas hésiter à dire bonjour, je ne suis pas ton chien...")
