8ee74740d5a196ed9909336eab5535a9f5d29247
Kenza <kenza.dieye@gmail.com>
2018-06-06 15:41:26 +0200

    test reponse question 1

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -11,9 +11,9 @@ Bot.on :message do |message|
   message.sent_at     # => 2016-04-22 21:30:36 +0200
   message.text        # => 'Hello, bot!'
   message.attachments # => [ { 'type' => 'image', 'payload' => { 'url' => 'https://www.example.com/1.jpg' } } ]
+  message.mark_seen
   message.typing_on
-  message.reply(text: "Bonjour je m’appelle Delphos. Je suis ici pour t'aider
-    à vivre mieux et pour vivre sans stress. Comment te sens-tu aujourd’hui ?",
+  message.reply(text: "Bonjour je m’appelle Delphos. Je suis ici pour t'aider à vivre mieux et pour vivre sans stress. Comment te sens-tu aujourd’hui ?",
     quick_replies: [
       {
         content_type: 'text',
@@ -32,4 +32,15 @@ Bot.on :message do |message|
       }
     ]
     )
+end
+
+Bot.on :postback do |postback|
+  postback.sender    # => { 'id' => '1008372609250235' }
+  postback.recipient # => { 'id' => '2015573629214912' }
+  postback.sent_at   # => 2016-04-22 21:30:36 +0200
+  postback.payload   # => 'EXTERMINATE'
+
+  if postback.payload == '1'
+    puts "Ça tombe bien #{postback.recipient}, je suis là pour t'aider ! Qu'est ce qui cause ton stress en ce moment ?"
+  end
 end
