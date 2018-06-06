a32ccc3cf39203d2035f22cbde6048f3b32bed53
Kenza <kenza.dieye@gmail.com>
2018-06-06 16:50:46 +0200

    test1087875

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -11,31 +11,31 @@ Bot.on :message do |message|
   # message.sent_at     # => 2016-04-22 21:30:36 +0200
   # message.text        # => 'Hello, bot!'
   # message.attachments # => [ { 'type' => 'image', 'payload' => { 'url' => 'https://www.example.com/1.jpg' } } ]
-  message.mark_seen
-  message.typing_on
-  message.reply(text: "Bonjour je m’appelle Delphos. Je suis ici pour t'aider à vivre mieux et pour vivre sans stress. Comment te sens-tu aujourd’hui ?",
-    quick_replies: [
-      {
-        content_type: 'text',
-        title: 'Sévèrement stressé(e)!',
-        payload: '1'
-      },
-      {
-        content_type: 'text',
-        title: 'Moyennement stressé(e)!',
-        payload: '2'
-      },
-      {
-        content_type: 'text',
-        title: 'Peu/pas stressé(e)!',
-        payload: '3'
-      }
-    ]
-    )
-end
-
-Bot.on :message do |message|
   if message.text.include? 'Sévèrement'
+    message.mark_seen
+    message.typing_on
     message.reply(text: "Ça tombe bien, je suis là pour t'aider ! Qu'est ce qui cause ton stress en ce moment ?")
+  else
+    message.mark_seen
+    message.typing_on
+    message.reply(text: "Bonjour je m’appelle Delphos. Je suis ici pour t'aider à vivre mieux et pour vivre sans stress. Comment te sens-tu aujourd’hui ?",
+      quick_replies: [
+        {
+          content_type: 'text',
+          title: 'Sévèrement stressé(e)!',
+          payload: '1'
+        },
+        {
+          content_type: 'text',
+          title: 'Moyennement stressé(e)!',
+          payload: '2'
+        },
+        {
+          content_type: 'text',
+          title: 'Peu/pas stressé(e)!',
+          payload: '3'
+        }
+      ]
+      )
   end
 end
